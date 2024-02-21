486cdd913a026f18cf6461d7dc091b8cfd10adf3
Giovane <giovane.magri@gmail.com>
2024-02-20 21:56:03 -0300

    pré-processamento dos dados

--- cody.py
+++ cody.py
@@ -1,8 +1,33 @@
+#Importação de bibliotecas
 import spacy
 import string
 import random
 import pandas as pd
 import seaborn as sns
 import numpy as np
+from spacy.lang.pt.stop_words import STOP_WORDS
 
-base_dados = pd.read_csv(r'C:\Users\giova\OneDrive\Documentos\Programas\Github\Classificador-de-Sentimentos\Train50.csv', encoding= 'utf-8')
+#Criando as variáveis
+base_dados = pd.read_csv(r'Train50.csv', encoding = 'utf-8', sep = ';')
+pontuacoes = string.punctuation
+stop_words = STOP_WORDS
+
+#pln = processamento de linguagem natural
+pln = spacy.load('pt_core_news_sm')
+
+def preprocessamento(texto):
+
+    texto = texto.lower()
+    documento = pln(texto)
+    lista = []
+
+    for token in documento:
+        lista.append(token.lemma_)
+    
+    lista = [palavra for palavra in lista if palavra not in stop_words and palavra not in pontuacoes]
+    lista = ' '.join([str(elemento) for elemento in lista if not elemento.isdigit()])
+
+    return lista
+
+teste = preprocessamento('Estou aprendendo processamento de linguagem natural, curso em Curitiba')
+print(teste)
