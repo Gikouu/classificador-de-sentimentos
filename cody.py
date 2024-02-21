3149bd64e849f08d3fc19c1c5b830f4e7990d816
Giovane <giovane.magri@gmail.com>
2024-02-20 21:11:44 -0300

    base de dados

--- cody.py
+++ cody.py
@@ -1,1 +1,8 @@
-print("Hello")
+import spacy
+import string
+import random
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+base_dados = pd.read_csv(r'C:\Users\giova\OneDrive\Documentos\Programas\Github\Classificador-de-Sentimentos\Train50.csv', encoding= 'utf-8')
