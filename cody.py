d1858ee3f984b08f00e1349ab7f844d6a05b992e
Giovane <giovane.magri@gmail.com>
2024-02-20 23:06:49 -0300

    algoritmo de treinamento

--- cody.py
+++ cody.py
@@ -1,4 +1,5 @@
 #Importação de bibliotecas
+
 import spacy
 import string
 import random
@@ -6,15 +7,29 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 from spacy.lang.pt.stop_words import STOP_WORDS
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
 
 #Criando as variáveis
+#pln = processamento de linguagem natural
+
+@Language.factory("categorias")
+def get_lang_detector(nlp, name):
+   return LanguageDetector()
+
 base_dados = pd.read_csv(r'Train50.csv', encoding = 'utf-8', sep = ';')
 pontuacoes = string.punctuation
 stop_words = STOP_WORDS
 base_dados_final = []
-
-#pln = processamento de linguagem natural
 pln = spacy.load('pt_core_news_sm')
+modelo = spacy.blank('pt')
+categorias = modelo.create_pipe('textcat')
+categorias.add_label('ALEGRIA')
+categorias.add_label('MEDO')
+modelo.add_pipe('categorias', last=True)
+historico = []
+
+#ajustando o banco de dados
 
 def preprocessamento(texto):
 
@@ -34,8 +49,22 @@ base_dados['tweet_text'] = base_dados['tweet_text'].apply(preprocessamento)
 
 for texto, emocao in zip(base_dados['tweet_text'], base_dados['sentiment']):
     if emocao == 1:
-        dic = ({'ALEGRIA':True, 'TRISTEZA':False})
+        dic = ({'ALEGRIA':True, 'MEDO':False})
     elif emocao == 0:
-        dic = ({'ALEGRIA':False, 'TRISTEZA':True})
+        dic = ({'ALEGRIA':False, 'MEDO':True})
     
     base_dados_final.append([texto, dic.copy()])
+
+#treinamento
+    
+modelo.begin_training()
+for epoca in range(10):
+    random.shuffle(base_dados_final)
+    losses = {}
+    for batch in spacy.util.minibatch(base_dados_final, 50):
+        textos = [modelo(texto) for texto, entities in batch]
+        annotations = [{'cats': entities} for texto, entities in batch]
+        modelo.update(textos, annotations, losses=losses)
+        if epoca <= 10:
+            print(losses)
+            historico.append(losses)
